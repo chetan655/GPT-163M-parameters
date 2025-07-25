81f23311a38c56d05e900ad61cc1ee8a48abe26d
chetan <alexantar96@gmail.com>
2025-07-26 00:14:56 +0530

    config

---
+++ config.py
@@ -0,0 +1,17 @@
+
+
+cfg = {
+    "vocab_size": 50256,
+    "n_layers": 12,
+    "stride" : 1024,
+    "context_length" : 1024,
+    "emb_dim": 512,
+    "n_heads": 8,
+    "dropout" : 0.1,
+    "learning_rate": 0.0004,
+    "qkv_bias": False,
+    "max_new_tokens": 50,
+    "eval_iter": 5,
+    "eval_freq": 5,
+    "num_epochs": 3
+}
